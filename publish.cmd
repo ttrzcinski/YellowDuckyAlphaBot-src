2fc660cb4cb3eac40c683cfa3dcc73c8dd270ffb
Tomasz Trzcinski <tomasz.trzcinski@capgemini.com>
2018-08-03 09:16:03 +0200

    Just generated it on Azure portal.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=yellowduckyalphabot-Web-Deploy.pubxml -p:Password=9QyG0nSrfD2m68lah9nsgiBrXtrJfpxWzNgl7gJdYB84XNQ5QcPpGMEZHmWH
+
